5d38c1b989d649c2455a6823594f9a119702ecfd
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-02-05 17:56:01 +0530

    add sweetalert

--- src/page/game/index.jsx
+++ src/page/game/index.jsx
@@ -1,47 +1,133 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-
-// Use this api :https://marcconrad.com/uob/smile/api.php?out=json and get the question and display  the question as image and store the solution
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 export default function Index() {
   //fetch the api and store the question and solution
   const [question, setQuestion] = useState(null);
   const [solution, setSolution] = useState(null);
   const [loading, setLoading] = useState(false);
-  // const [answer, setAnswer] = UseState(null);
-  // const [score, setScore] = UseState(0);
-  
+  const [score, setScore] = useState(0);
+  const [count, setCount] = useState(0);
+  const [userName, setUserName] = useState("Your");
+//Onload fetch the data
+window.onload = function () {
+  getUserName();  
+  fetchData();
+}
+ function fetchData(){
+    fetch("https://marcconrad.com/uob/smile/api.php?out=json")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestion(data.question);
+        setSolution(data.solution);
+        setLoading(true);
+        console.log(data);
+      });
+  }
 
-  // await
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(
-        "https://marcconrad.com/uob/smile/api.php?out=json"
-      );
-      const data = await response.json();
-      setQuestion(data.question);
-      setSolution(data.solution);
-      // console.log(data);
-      // console.log(data.question);
-      // console.log(data.solution);
-      setLoading(true);
-    }
-    fetchData();
-  }, []);
 
   // check the answer
   function checkAnswer(answer) {
     console.log("Answer", answer);
     console.log("Solution", solution);
-    console.log("Correct", answer === solution);
-  }
 
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You have selected : " + answer + "",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirm",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (answer.toString() === solution.toString()) {
+          setScore(score + 10);
+          Swal.fire("Correct!", "+10 points added : ", "success");
+          showScore(10);
+        } else {
+          if(count === 5){
+            fetchData();
+          }
+          setCount(count + 1);
+          Swal.fire("Wrong!", "0 points", "error");
+        }
+      }
+    });
+  }
+  function showScore(value) {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: ""+userName+" Score is : " + (score+value) + "",
+      showConfirmButton: false,
+      timer: 2000,
+    }).then((result) => {
+      fetchData();
+    });
+  }
+  function hint() {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Answer : " + solution + "",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirm",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setScore(score + 5);
+        Swal.fire("Correct!", "-5 points deducted : ", "success");
+        showScore(5);
+      }
+    });
+  }
+function next(){
+  fetchData();
+}
+function getUserName(){
+  Swal.fire({
+    title: 'Submit your Github username',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Look up',
+    showLoaderOnConfirm: true,
+    preConfirm: (login) => {
+      return fetch(`//api.github.com/users/${login}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        })
+        .catch(error => {
+          Swal.showValidationMessage(
+            `Request failed: ${error}`
+          )
+        })
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value.login}'s avatar`,
+        imageUrl: result.value.avatar_url
+      })
+      setUserName(result.value.login)
+    }
+  })
+}
   return (
-    <div className="container pt-5">
+    <div className="container pt-4">
+
       <div className="card text-center">
         <div className="card-header">SMILE GAME</div>
         <div className="card-body">
-          <h5 className="card-title">Question</h5>          
+          <h5 className="card-title">Question</h5>
           {loading ? (
             <img src={question} className="img-thumbnail" alt="question" />
           ) : (
@@ -49,7 +135,7 @@ export default function Index() {
           )}
         </div>
         <div className="card-footer ">
-        <h5 className="card-title">Solution</h5>
+          <h5 className="card-title">Guess the Solution?</h5>
           <div className="row d-flex justify-content-center">
             <div className="col-2 pt-3">
               <input
@@ -82,7 +168,7 @@ export default function Index() {
                 type="button"
                 value="3"
                 className="btn btn-outline-dark"
-                onClick={() => checkAnswer(1)}
+                onClick={(e) => checkAnswer(e.target.value)}
               />
             </div>
 
@@ -138,6 +224,14 @@ export default function Index() {
                 onClick={(e) => checkAnswer(e.target.value)}
               />
             </div>
+          </div>
+          <div className="d-grid gap-2 col-6 mx-auto pt-4">
+            <button className="btn btn-outline-info" type="button" onClick={hint}>
+              Hint
+            </button>
+            <button className="btn btn-outline-danger" type="button" onClick={next}>
+              Next
+            </button>
           </div>
         </div>
       </div>
